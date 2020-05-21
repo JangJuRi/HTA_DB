0c9ef0b43b1c61752c697e9de872b1051868842d
JangJuRi <wkdwnfl119@naver.com>
2020-05-21 20:21:20 +0900

    0521

---
+++ 0521.sql
@@ -0,0 +1,23 @@
+-- 최근 입고된 도서
+-- 책번호 순으로 내림차순 정렬했을 때 5번째 책까지 조회하기
+select book_no, book_title, book_writer, book_price
+from (select book_no, book_title, book_writer, book_price
+      from sample_books
+      order by book_no desc)
+where ROWNUM <= 5;
+
+-- 최근 가입한 사용자
+-- 가입일 순으로 내림차순 정렬했을 때 5번째 사용자까지 조회하기
+select user_name, user_id, user_email, user_registered_date
+from (select user_name, user_id, user_email, user_registered_date
+      from sample_book_users
+      order by user_registered_date desc)
+where ROWNUM <= 5;
+
+
+
+select book_no, book_title, book_writer, book_publisher, book_point,
+       (select count(*) from sample_book_reviews where book_no = A.book_no) review_cnt,
+       book_price, book_discount_price, book_likes, book_stock
+from sample_books A
+where book_no = 10024;
